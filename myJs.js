ca4d330e404e9cb6be90b7110487f0d306459035
Oks777 <103444846+Oks777@users.noreply.github.com>
2022-06-12 16:45:05 +0300

    Update myJs.js

--- myJs.js
+++ myJs.js
@@ -1,8 +1,26 @@
-console.log('hello js!!')
+// console.log('hello js!!')
+
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Перед скриптом...</p>
+
+  <script>
+    alert( 'Привет, мир!' );
+  </script>
+
+  <p>...После скрипта.</p>
+
+</body>
+
+</html>
 
 
 
+// Поместить скрипт в отдельный файл с помощью атрибута src
 
-
+<script src="/path/to/script.js"></script>
 
 
